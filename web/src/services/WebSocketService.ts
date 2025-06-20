ad0db84655a34d5436aa6c4630c50731d4b52f15
Jeff Son <sonjeff@naver.com>
2025-06-20 15:49:13 +0900

    WebSocketService.ts 수정: SockJS 웹소켓 URL을 환경 변수에 따라 동적으로 설정하도록 변경

--- web/src/services/WebSocketService.ts
+++ web/src/services/WebSocketService.ts
@@ -19,7 +19,10 @@ class WebSocketService {
 
   constructor() {
     this.client = new Client({
-      webSocketFactory: () => new SockJS("http://localhost:8080/ws"),
+      webSocketFactory: () =>
+        new SockJS(
+          `${process.env.REACT_APP_API_URL || "http://localhost:8080"}/ws`
+        ),
       debug: (str) => {
         console.log("STOMP Debug:", str);
       },
